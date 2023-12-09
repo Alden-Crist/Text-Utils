26b207149a104411567bb620c312d45644e780d8
Alden-Crist <aldencristrego08@gmail.com>
2023-12-09 11:41:30 +0530

    Updates

---
+++ precache-manifest.e3d8068a6416d036b9d15fc85d8c666a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee344f6110987100db47119a4a5b8c96",
+    "url": "/Text-Utils/index.html"
+  },
+  {
+    "revision": "9b1f8887d214d89e3865",
+    "url": "/Text-Utils/static/css/main.6c059652.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/Text-Utils/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Text-Utils/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/Text-Utils/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "9b1f8887d214d89e3865",
+    "url": "/Text-Utils/static/js/main.265f503d.chunk.js"
+  },
+  {
+    "revision": "70535f5d85e235be21b4",
+    "url": "/Text-Utils/static/js/runtime-main.8ad2471e.js"
+  }
+]);
